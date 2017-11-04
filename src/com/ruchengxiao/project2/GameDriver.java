d221712669cde4c5af602bd092986076ae97af7f
ruchengxiao <31635807+ruchengxiao@users.noreply.github.com>
2017-11-03 23:26:04 -0700

    Modified

---
+++ src/com/ruchengxiao/project2/GameDriver.java
@@ -0,0 +1,17 @@
+package com.ruchengxiao.project2;
+
+import java.util.Scanner;
+
+public class GameDriver {
+
+    public static void main(String[] args) {
+        Game test = new Game(4);
+        test.playAGame();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.next().equals("y")) {
+            test = new Game(4);
+            test.playAGame();
+        }
+    }
+}
+ 
